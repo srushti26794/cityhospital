6a867e7bac2cbcb30540fd2a997be2e6bfbbd153
srushti26794 <srushtichangela267@gmail.com>
2023-12-09 15:59:32 +0530

    done

--- src/Container/LoginSignup/LoginSignup.js
+++ src/Container/LoginSignup/LoginSignup.js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { Container } from 'reactstrap';
 import "./LoginSignup.css"
+import { useFormik } from 'formik';
 
 function LoginSignup(props) {
     const [type, setType] = useState('login');
@@ -14,6 +15,10 @@ function LoginSignup(props) {
     const handleLogin = () => {
         setType('login');
         // console.log(type);
+    }
+
+    const handleForgot = () => {
+        setType('forgot');
     }
 
     console.log(type);
@@ -32,25 +37,39 @@ function LoginSignup(props) {
                 <form action method='post' className='SignupForm'>
                     <div className='row'>
                         <div className='sign_up'>
-                                {
-                                    type === 'signup'
+                            {
+                                type === 'signup'
                                     ? <><input type="text" name="name" className="form-control signup" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                                    <div className="validate" /></> : null
-                                }
-                            
+                                        <div className="validate" /></> : null
+                            }
+
 
                             <input type="email" className="form-control signup" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
                             <div className="validate" />
 
-                            <input type="password" className="form-control signup" name="password" id="password" placeholder="Enter Password" data-rule="password" data-msg="Please enter a valid password" />
-                            <div className="validate" />
+                            {
+                                type === 'forgot'
+                                    ? null
+                                    : <><input type="password" className="form-control signup" name="password" id="password" placeholder="Enter Password" data-rule="password" data-msg="Please enter a valid password" />
+                                        <div className="validate" /></>
+                            }
 
-                            <div className="text-center"><button className='signUpBtn' type="submit">Sign up</button></div>
-                            <p className='haveAcc'>Already have an account? <a href='#' onClick={handleLogin}>Login</a></p>
 
-                            <div className="text-center"><button className='loginBtn' type="submit">Login</button></div>
-                            <p className='haveAcc'><a href='#'>Forgot password?</a></p>
-                            <p><a href='#' onClick={handleSignup}>Create a new account?</a></p>
+                            {
+                                type === 'forgot'
+                                    ? <><div className="text-center"><button className='signUpBtn' type="submit">Find account</button></div>
+                                    <p className='haveAcc'><a href='#' onClick={handleLogin}>Login</a></p></>
+
+                                        : type === 'signup'
+                                            ? <><div className="text-center"><button className='signUpBtn' type="submit">Sign up</button></div>
+                                                <p className='haveAcc'>Already have an account? <a href='#' onClick={handleLogin}>Login</a></p></>
+                                            : <><div className="text-center"><button className='loginBtn' type="submit">Login</button></div>
+                                                <p className='haveAcc'><a href='#' onClick={handleForgot}>Forgot password?</a></p>
+                                                <p><a href='#' onClick={handleSignup}>Create a new account?</a></p></>
+                            }
+
+
+
                         </div>
                     </div>
                 </form>
