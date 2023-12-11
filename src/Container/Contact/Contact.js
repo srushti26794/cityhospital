1cfc72745ee06f01a4a310f7ba6fb539c798eb78
srushti26794 <srushtichangela267@gmail.com>
2023-12-11 11:20:41 +0530

    done

--- src/Container/Contact/Contact.js
+++ src/Container/Contact/Contact.js
@@ -1,6 +1,33 @@
+import { useFormik } from 'formik';
 import React from 'react';
+import * as yup from 'yup';
 
 function Contact(props) {
+
+    const contactSchema = yup.object({
+        name : yup.string().required("Please enter name").matches(/^[a-zA-Z ]{2,30}$/, "Please enter valid name"),
+        email : yup.string().required("Please enter email").email("Please enter valid email"),
+        subject : yup.string().required("Please enter subject"),
+        message : yup.string("Please enter message").required().min(5, "Enter minimun 5 letter").max(10, "Only 10 letter allowed")
+    })
+
+    const formikObj = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            subject: '',
+            message: ''
+        },
+        validationSchema: contactSchema,
+        onSubmit: values => {
+         console.log(values);
+        },
+      });
+
+      let {handleSubmit, handleChange, handleBlur, touched, errors, values} = formikObj
+
+      console.log(touched, errors);
+
     return (
         <section id="contact" className="contact">
             <div className="container">
@@ -33,21 +60,29 @@ function Contact(props) {
                         </div>
                     </div>
                     <div className="col-lg-8 mt-5 mt-lg-0">
-                        <form action method="post" role="form" className="php-email-form">
+                        <form onSubmit={handleSubmit} method="post" role="form" className="php-email-form">
                             <div className="row">
                                 <div className="col-md-6 form-group">
-                                    <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" required />
+                                    <input type="text" name="name" className="form-control" id="name" placeholder="Your Name" onChange={handleChange} onBlur={handleBlur} value={values.name}/>
+                                    <span>{errors.name && touched.name ? errors.name : null}</span>
                                 </div>
+                                
                                 <div className="col-md-6 form-group mt-3 mt-md-0">
-                                    <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" required />
+                                    <input type="email" className="form-control" name="email" id="email" placeholder="Your Email" onChange={handleChange} onBlur={handleBlur} value={values.email}/>
+                                    <span>{errors.email && touched.email ? errors.email : null}</span>
                                 </div>
+                                
                             </div>
                             <div className="form-group mt-3">
-                                <input type="text" className="form-control" name="subject" id="subject" placeholder="Subject" required />
+                                <input type="text" className="form-control" name="subject" id="subject" placeholder="Subject" onChange={handleChange} onBlur={handleBlur} value={values.subject} />
+                                <span>{errors.subject && touched.subject ? errors.subject : null}</span>
                             </div>
+                            
                             <div className="form-group mt-3">
-                                <textarea className="form-control" name="message" rows={5} placeholder="Message" required defaultValue={""} />
+                                <textarea className="form-control" name="message" rows={5} placeholder="Message" onChange={handleChange} onBlur={handleBlur} value={values.message} />
+                                <span>{errors.message && touched.message ? errors.message : null}</span>
                             </div>
+                            
                             <div className="my-3">
                                 <div className="loading">Loading</div>
                                 <div className="error-message" />
--- src/Container/LoginSignup/LoginSignup.js
+++ src/Container/LoginSignup/LoginSignup.js
@@ -1,18 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Container } from 'reactstrap';
 import "./LoginSignup.css"
-import { Formik } from 'formik';
 import * as Yup from "yup";
 
-
-const schema = Yup.object().shape({
-    email: Yup.string()
-      .required("Email is a required field")
-      .email("Invalid email format"),
-    password: Yup.string()
-      .required("Password is a required field")
-      .min(8, "Password must be at least 8 characters"),
-  });
 
 function LoginSignup(props) {
     const [type, setType] = useState('login');
