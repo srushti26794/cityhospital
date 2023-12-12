8279d7ad8045fef192f2a875074f1235fab626ee
srushti26794 <srushtichangela267@gmail.com>
2023-12-12 11:30:58 +0530

    done

--- src/Container/Appointment/Appointment.js
+++ src/Container/Appointment/Appointment.js
@@ -4,104 +4,129 @@ import * as yup from 'yup';
 
 function Appointment(props) {
 
+    let d = new Date();
+
+    let nd = new Date();
+    nd.setDate(d.getDate() - 1)
+
+    const MAX_FILE_SIZE = 102400; //100KB
+
+    const validFileExtensions = { image: ['jpg', 'gif', 'png', 'jpeg', 'svg', 'webp'] };
+
+    function isValidFileType(fileName, fileType) {
+        return fileName && validFileExtensions[fileType].indexOf(fileName.split('.').pop()) > -1;
+    }
+
     const appointSchema = yup.object({
-        name : yup.string().required("Enter your name").matches(/^[a-zA-Z ]{2,30}$/, "Please enter valid name"),
-        email : yup.string().required("Enter your email").email("Enter valid email"),
-        phone : yup.string().required("Enter your phone number").matches('^([9]{1})([1-9]{1})([0-9]{8})$',"Please enter valid phone number"),
-        date : yup.date().min(new Date(),"Date must be in future").required("Please select date"),
-        department : yup.string().required("Enter department"),
-        file : yup.string().required("Please upload prescription photo"),
-        message : yup.string().required("Enter message")
-        .test('word-count', 'Must have at least 5 words', value => {
-          const count = value.trim().split(/\s+/).length;
-          return count >= 5;})
-          .test('word-count', 'Maximum 10 words allowed', value => {
-            const count = value.trim().split(/\s+/).length;
-            return count <= 10;})
-    })
+        name: yup.string().required("Enter your name").matches(/^[a-zA-Z ]{2,30}$/, "Please enter valid name"),
+        email: yup.string().required("Enter your email").email("Enter valid email"),
+        phone: yup.string().required("Enter your phone number").matches('^([9]{1})([1-9]{1})([0-9]{8})$', "Please enter valid phone number"),
+        date: yup.date().min(nd, "Date must be current or future").required("Please select date"),
+        department: yup.string().required("Enter department"),
+        file: yup
+            .mixed()
+            .required("Required")
+            .test("is-valid-type", "Not a valid image type",
+                value => isValidFileType(value && value.name, "image"))
+            .test("is-valid-size", "Max allowed size is 100KB",
+                value => value && value.size <= MAX_FILE_SIZE),
+    
+    message: yup.string()
+        .required("Enter message")
+        .test('message', "Message in between 5 to 10 word", function (val) {
+            let array = val.split(" ");
 
-    let formikObj = useFormik({
-        initialValues: {
-            name : '',
-            email : '',
-            phone : '',
-            date : '',
-            department : '',
-            file : '',
-            message : ''
-        },
-        validationSchema: appointSchema,
-        onSubmit: values => {
-         console.log(values);
-        },
-    })
+            if (array.length >= 5 && array.length <= 10) {
+                return true;
+            } else {
+                return false
+            }
+        })
+})
 
-    let {handleSubmit, handleChange, handleBlur, touched, errors, values} = formikObj;
+let formikObj = useFormik({
+    initialValues: {
+        name: '',
+        email: '',
+        phone: '',
+        date: '',
+        department: '',
+        file: '',
+        message: ''
+    },
+    validationSchema: appointSchema,
+    onSubmit: values => {
+        console.log(values);
+    },
+})
 
-    return (
-        <section id="appointment" className="appointment">
-            <div className="container">
-                <div className="section-title">
-                    <h2>Make an Appointment</h2>
-                    <p>Aenean enim orci, suscipit vitae sodales ac, semper in ex. Nunc aliquam eget nibh eu euismod. Donec dapibus
-                        blandit quam volutpat sollicitudin. Fusce tincidunt sit amet ex in volutpat. Donec lacinia finibus tortor.
-                        Curabitur luctus eleifend odio. Phasellus placerat mi et suscipit pulvinar.</p>
-                </div>
-                <form onSubmit={handleSubmit} method="post" role="form" className="php-email-form">
-                    <div className="row">
-                        <div className="col-md-4 form-group">
-                            <input onChange={handleChange} onBlur={handleBlur} value={values.name} type="text" name="name" className="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                            <span>{errors.name && touched.name ? errors.name : null}</span>
-                            <div className="validate" />
-                        </div>
-                        <div className="col-md-4 form-group mt-3 mt-md-0">
-                            <input onChange={handleChange} onBlur={handleBlur} value={values.email} type="email" className="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
-                            <span>{errors.email && touched.email ? errors.email : null}</span>
-                            <div className="validate" />
-                        </div>
-                        <div className="col-md-4 form-group mt-3 mt-md-0">
-                            <input onChange={handleChange} onBlur={handleBlur} value={values.phone} type="number" className="form-control" name="phone" id="phone" placeholder="Your Phone" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                            <span>{errors.phone && touched.phone ? errors.phone : null}</span>
-                            <div className="validate" />
-                        </div>
-                    </div>
-                    <div className="row">
-                        <div className="col-md-4 form-group mt-3">
-                            <input onChange={handleChange} onBlur={handleBlur} value={values.date} type="date" name="date" className="form-control datepicker" id="date" placeholder="Appointment Date" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
-                            <span>{errors.date && touched.date ? errors.date : null}</span>
-                            <div className="validate" />
-                        </div>
-                        <div className="col-md-4 form-group mt-3">
-                            <select onChange={handleChange} onBlur={handleBlur} value={values.department} name="department" id="department" className="form-select">
-                                <option value>Select Department</option>
-                                <option value="Department 1">Department 1</option>
-                                <option value="Department 2">Department 2</option>
-                                <option value="Department 3">Department 3</option>
-                            </select>
-                            <span>{errors.department && touched.department ? errors.department : null}</span>
-                            <div className="validate" />
-                        </div>
-                        <div className="col-md-4 form-group mt-3">
-                            <input onChange={handleChange} onBlur={handleBlur} value={values.file} type='file' name='file'></input>
-                            <div className="validate" />
-                        </div>
-                    </div>
-                    <div className="form-group mt-3">
-                        <textarea onChange={handleChange} onBlur={handleBlur} value={values.message} className="form-control" name="message" rows={5} placeholder="Message (Optional)" defaultValue={""} />
-                        <span>{errors.message && touched.message ? errors.message : null}</span>
+let { handleSubmit, handleChange, handleBlur, touched, errors, values } = formikObj;
+
+return (
+    <section id="appointment" className="appointment">
+        <div className="container">
+            <div className="section-title">
+                <h2>Make an Appointment</h2>
+                <p>Aenean enim orci, suscipit vitae sodales ac, semper in ex. Nunc aliquam eget nibh eu euismod. Donec dapibus
+                    blandit quam volutpat sollicitudin. Fusce tincidunt sit amet ex in volutpat. Donec lacinia finibus tortor.
+                    Curabitur luctus eleifend odio. Phasellus placerat mi et suscipit pulvinar.</p>
+            </div>
+            <form onSubmit={handleSubmit} method="post" role="form" className="php-email-form">
+                <div className="row">
+                    <div className="col-md-4 form-group">
+                        <input onChange={handleChange} onBlur={handleBlur} value={values.name} type="text" name="name" className="form-control" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                        <span>{errors.name && touched.name ? errors.name : null}</span>
                         <div className="validate" />
                     </div>
-                    <div className="mb-3">
-                        <div className="loading">Loading</div>
-                        <div className="error-message" />
-                        <div className="sent-message">Your appointment request has been sent successfully. Thank you!</div>
+                    <div className="col-md-4 form-group mt-3 mt-md-0">
+                        <input onChange={handleChange} onBlur={handleBlur} value={values.email} type="email" className="form-control" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                        <span>{errors.email && touched.email ? errors.email : null}</span>
+                        <div className="validate" />
                     </div>
-                    <div className="text-center"><button type="submit">Make an Appointment</button></div>
-                </form>
-            </div>
-        </section>
+                    <div className="col-md-4 form-group mt-3 mt-md-0">
+                        <input onChange={handleChange} onBlur={handleBlur} value={values.phone} type="number" className="form-control" name="phone" id="phone" placeholder="Your Phone" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                        <span>{errors.phone && touched.phone ? errors.phone : null}</span>
+                        <div className="validate" />
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-4 form-group mt-3">
+                        <input onChange={handleChange} onBlur={handleBlur} value={values.date} type="date" name="date" className="form-control datepicker" id="date" placeholder="Appointment Date" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                        <span>{errors.date && touched.date ? errors.date : null}</span>
+                        <div className="validate" />
+                    </div>
+                    <div className="col-md-4 form-group mt-3">
+                        <select onChange={handleChange} onBlur={handleBlur} value={values.department} name="department" id="department" className="form-select">
+                            <option value>Select Department</option>
+                            <option value="Department 1">Department 1</option>
+                            <option value="Department 2">Department 2</option>
+                            <option value="Department 3">Department 3</option>
+                        </select>
+                        <span>{errors.department && touched.department ? errors.department : null}</span>
+                        <div className="validate" />
+                    </div>
+                    <div className="col-md-4 form-group mt-3">
+                        <input onChange={handleChange} onBlur={handleBlur} value={values.file} type='file' name='file' className="form-control datepicker"></input>
+                        <span>{errors.file && touched.file ? errors.file : null}</span>
+                        <div className="validate" />
+                    </div>
+                </div>
+                <div className="form-group mt-3">
+                    <textarea onChange={handleChange} onBlur={handleBlur} value={values.message} className="form-control" name="message" rows={5} placeholder="Message (Optional)" defaultValue={""} />
+                    <span>{errors.message && touched.message ? errors.message : null}</span>
+                    <div className="validate" />
+                </div>
+                <div className="mb-3">
+                    <div className="loading">Loading</div>
+                    <div className="error-message" />
+                    <div className="sent-message">Your appointment request has been sent successfully. Thank you!</div>
+                </div>
+                <div className="text-center"><button type="submit">Make an Appointment</button></div>
+            </form>
+        </div>
+    </section>
 
-    );
+);
 }
 
 export default Appointment;
--- src/Container/LoginSignup/LoginSignup.js
+++ src/Container/LoginSignup/LoginSignup.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import { Container } from 'reactstrap';
 import "./LoginSignup.css"
-import * as Yup from "yup";
+import * as yup from 'yup';
+import { useFormik } from 'formik';
 
 
 function LoginSignup(props) {
@@ -10,19 +11,41 @@ function LoginSignup(props) {
 
     const handleSignup = () => {
         setType('signup')
-        // console.log(type);
     }
 
     const handleLogin = () => {
         setType('login');
-        // console.log(type);
     }
 
     const handleForgot = () => {
         setType('forgot');
     }
 
-    console.log(type);
+    const loginSignupSchema = yup.object({
+        name: yup.string().required("Enter your name").matches(/^[a-zA-Z ]{2,30}$/, "Please enter valid name"),
+        email: yup.string().required("Enter your email").email("Enter valid email"),
+        password: yup.string().min(8, 'Password must be 8 characters long')
+            .matches(/[0-9]/, 'Password requires a number')
+            .matches(/[a-z]/, 'Password requires a lowercase letter')
+            .matches(/[A-Z]/, 'Password requires an uppercase letter')
+            .matches(/[^\w]/, 'Password requires a symbol'),
+        confPassword: yup.string().oneOf([yup.ref('password'), null], 'Passwords must match')
+    })
+
+    let formikObj = useFormik({
+        initialValues: {
+            name: '',
+            email: '',
+            password: '',
+            confPassword: ''
+        },
+        validationSchema: loginSignupSchema,
+        onSubmit: values => {
+            console.log(values);
+        },
+    })
+
+    let { handleSubmit, handleChange, handleBlur, touched, errors, values } = formikObj;
 
     return (
         <section id='LoginSignup'>
@@ -30,48 +53,52 @@ function LoginSignup(props) {
 
                 <div className="section-title">
                     {
-                        type === 'signup' ? <h2>Sign Up</h2> : <h2>Login</h2>
+                        type === 'forgot' ? <h2>Forgot password</h2> : type === 'signup' ? <h2>Sign Up</h2> : <h2>Login</h2>
                     }
 
                 </div>
 
-                <form action method='post' className='SignupForm'>
+                <form onSubmit={handleSubmit} method='post' className='SignupForm'>
                     <div className='row'>
-                        
+
                         <div className='sign_up'>
                             {
                                 type === 'signup'
-                                    ? <><input type="text" name="name" className="form-control signup" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                                    ? <><input onChange={handleChange} onBlur={handleBlur} value={values.name} type="text" name="name" className="form-control signup" id="name" placeholder="Your Name" data-rule="minlen:4" data-msg="Please enter at least 4 chars" />
+                                        <span>{errors.name && touched.name ? errors.name : null}</span>
                                         <div className="validate" /></> : null
                             }
 
 
-                            <input type="email" className="form-control signup" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                            <input onChange={handleChange} onBlur={handleBlur} value={values.email} type="email" className="form-control signup" name="email" id="email" placeholder="Your Email" data-rule="email" data-msg="Please enter a valid email" />
+                            <span>{errors.email && touched.email ? errors.email : null}</span>
                             <div className="validate" />
 
                             {
                                 type === 'forgot'
                                     ? null
-                                    : <><input type="password" className="form-control signup" name="password" id="password" placeholder="Enter Password" data-rule="password" data-msg="Please enter a valid password" />
+                                    : <><input onChange={handleChange} onBlur={handleBlur} value={values.password} type="password" className="form-control signup" name="password" id="password" placeholder="Enter Password" data-rule="password" data-msg="Please enter a valid password" />
+                                        <span>{errors.password && touched.password ? errors.password : null}</span>
                                         <div className="validate" /></>
                             }
 
+                            {
+                                type === 'signup' ? <><input onChange={handleChange} onBlur={handleBlur} value={values.confPassword} type="password" className="form-control signup" name="confPassword" id="confPassword" placeholder="Confirm Password" data-rule="password" data-msg="Please enter a valid password" />
+                                    <span>{errors.confPassword && touched.confPassword ? errors.confPassword : null}</span><div className="validate" /></> : null
+                            }
 
                             {
                                 type === 'forgot'
                                     ? <><div className="text-center"><button className='signUpBtn' type="submit">Find account</button></div>
-                                    <p className='haveAcc'><a href='#' onClick={handleLogin}>Login</a></p></>
+                                        <p className='haveAcc'><a href='#' onClick={handleLogin}>Login</a></p></>
 
-                                        : type === 'signup'
-                                            ? <><div className="text-center"><button className='signUpBtn' type="submit">Sign up</button></div>
-                                                <p className='haveAcc'>Already have an account? <a href='#' onClick={handleLogin}>Login</a></p></>
-                                            : <><div className="text-center"><button className='loginBtn' type="submit">Login</button></div>
-                                                <p className='haveAcc'><a href='#' onClick={handleForgot}>Forgot password?</a></p>
-                                                <p><a href='#' onClick={handleSignup}>Create a new account?</a></p></>
+                                    : type === 'signup'
+                                        ? <><div className="text-center"><button className='signUpBtn' type="submit">Sign up</button></div>
+                                            <p className='haveAcc'>Already have an account? <a href='#' onClick={handleLogin}>Login</a></p></>
+                                        : <><div className="text-center"><button className='loginBtn' type="submit">Login</button></div>
+                                            <p className='haveAcc'><a href='#' onClick={handleForgot}>Forgot password?</a></p>
+                                            <p><a href='#' onClick={handleSignup}>Create a new account?</a></p></>
                             }
-
-
-
                         </div>
                     </div>
                 </form>
