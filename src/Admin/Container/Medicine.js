2d902472e6d70ab814e96550ee32362bbc244a17
srushti26794 <srushtichangela267@gmail.com>
2024-01-09 11:26:13 +0530

    done

--- src/Admin/Container/Medicine.js
+++ src/Admin/Container/Medicine.js
@@ -15,7 +15,6 @@ import { deleteMedicine, getMedicine, postMedicine, updateMedicine } from '../..
 import { useDispatch, useSelector } from 'react-redux';
 import CircularProgress from '@mui/material/CircularProgress';
 import Box from '@mui/material/Box';
-import { Typography } from '@mui/material';
 
 export default function Medicine() {
     const [medicineData, setMedicineData] = useState([])
